cae64c3d63fcf852617429d3e99f04498b231a6d
odmrs <omarcosviniciusdev@gmail.com>
2024-01-03 18:49:53 -0300

    Primeira parte das anotações e primeiros tipos primitivos

---
+++ 0-Fundamentos-do-C#/05-modulo/MeuApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MeuApp.Teste;
+
+namespace MeuApp
+{
+    // Classe principal do programa
+    class Program
+    {
+        // Função principal do programa
+        static void Main(string[] args)
+        {
+            /* Variaveis
+            // Definindo variaveis com tipo e com a palavra reservada var
+            // type name = value;
+            // int idade = 19;
+            // int idade = 19; // Inicializando, tornando o padrão
+            // var idade = 19; // Mesma coisa para aqui, vamos focar em colocar os tipos de cada valor
+            // var idade; // erro pois var necessita de uma inicialização, define assim que a variavel for iniciada.
+            */
+            /* Constantes
+            const int IDADE_MINIMA = 16;
+            */
+            // Tipos Primitivos
+
+            // Byte
+
+            // byte umByte = 127;
+
+            // Inteiros
+
+            // int numInt = -15;
+            // uint positivoNum = 15;
+            // short shortNum = 32000;
+            // ushort ushortNum = 64000;
+            // long longNum = 9000000;
+            // ulong ulongNum = 9999999999999999;
+
+            //  Números Reais
+
+            // float salario = 2.500f; 
+            // decimal salariozao = 2030210.232132m;
+            // double salriozin = 2.000;
+
+            // Bool
+
+            // bool usuarioJaCadastrado = true;
+            // bool usuariaNãoCadastrada = false;
+
+            // Char
+
+            // char homemOuMulher = 'H';
+            // char mulherOuHomem = 'M';
+
+            // String
+
+            // string primeiraLetra = "A";
+            // string nomeCompleto = "Marcos Vinícius";
+
+            // Var
+
+            // var idade = 25;
+
+        }
+    }
+}
