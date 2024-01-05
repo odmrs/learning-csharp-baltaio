6c2014b59af1e9e9794529f7d101adb5fbb46856
odmrs <omarcosviniciusdev@gmail.com>
2024-01-05 02:18:39 -0300

    adicionado uma muitos exemplos ao arquivo program.cs e mais uma parte das anotações do modulo 05

--- 0-Fundamentos-do-C#/05-modulo/MeuApp/Program.cs
+++ 0-Fundamentos-do-C#/05-modulo/MeuApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using MeuApp.Teste;
 
 namespace MeuApp
@@ -20,7 +21,7 @@ namespace MeuApp
             /* Constantes
             const int IDADE_MINIMA = 16;
             */
-            // Tipos Primitivos
+            /* Tipos Primitivos
 
             // Byte
 
@@ -63,19 +64,120 @@ namespace MeuApp
             // Console.WriteLine(idade);
 
             // int num = 25;
-            float numPonto = 25.6f;
-            // numPonto = num;
+            // float numPonto = 25.6f;
+            // // numPonto = num;
 
-            // int pegandoNum = int.Parse("100"); // Só espera string.
+            // // int pegandoNum = int.Parse("100"); // Só espera string.
 
-            // num = (int)numPonto; // Convertendo um float para inteiro.
+            // // num = (int)numPonto; // Convertendo um float para inteiro.
 
-            int inteiro = 0;
-            Console.WriteLine(numPonto);
-            Console.WriteLine(numPonto.ToString());
-            // inteiro = int.Parse(numPonto.ToString()); // Estamos transformando o numPonto para string e depois no parse transformando para inteiro. Pórem da erro pois o valor está com um caracter especial.
+            // int inteiro = 0;
+            // Console.WriteLine(numPonto);
+            // Console.WriteLine(numPonto.ToString());
+            // // inteiro = int.Parse(numPonto.ToString()); // Estamos transformando o numPonto para string e depois no parse transformando para inteiro. Pórem da erro pois o valor está com um caracter especial.
 
+            */
+            /* Operadores Aritméticos e de Atribuição
 
+            // Temos -> + - / *, ordem da matématica! Para mudar, só usar parenteses
+            // Console.WriteLine(22 / 4); // Pelo valor ser inteiro, e o resultado é um número real, o resultado será arredondado para cima!
+
+            // Atribuição
+
+            // int x = 5;
+
+            // x += 5; // x será 10, isso funciona para todos os outros operadores aritmeticos
+
+            // Console.WriteLine(x);
+
+            // Comparação
+
+            // Console.WriteLine(5 == 5); // Retorna apenas True ou False
+
+            // Operadores Lógicos
+
+            // bool condicao = (8 == 8) && (8 >= 8); // Retorna sempre um bool
+            */
+            /* Condicionais
+
+            // If - Else if - Else
+            // int idade = 89;
+
+            // if (idade >= 18 && idade < 65)
+            // {
+            //     Console.WriteLine("Maior de idade");
+            // }
+            // else if (idade >= 65)
+            // {
+            //     Console.WriteLine("Idosão");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Menor de idade");
+            // }
+
+            // Switch
+            string? name = null;
+            switch (name)
+            {
+                case "Marcos": Console.WriteLine("Gostoso"); break;
+                case "Bela": Console.WriteLine("Gostosa"); break;
+                case null: Console.WriteLine("Digita alguma coisa né amigão"); break;
+                default: Console.WriteLine("Saíiiii"); break;
+            }
+
+            */
+            /* Laços de Repetição: For
+
+            // For
+
+            // for (int i = 5; i >= 0; i--)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // While
+            // int value = 0;
+            // while (value <= 5)
+            // {
+            //     Console.WriteLine(value);
+            //     value++; // Caso não tenha incremento, vai gerar um loop
+            // }
+
+            // Do...While
+
+            // int value = 0;
+
+            // do
+            // {
+            //     Console.WriteLine(value); // vai executar pelo menos uma vez
+            //     value++;
+            // } while (value <= 5);
+            */
+            // Funções e Métodos
+
+            // Invocando uma função
+
+            // SayName("Marcos");
+
+            // Função com retorno
+            // string name = ReturnName("Marcos", "Vinicius");
+            Console.WriteLine(ReturnName("Marcos", "Gostosos"));
+
+        }
+
+        static void SayName(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        static string ReturnName(
+            string first,
+            string last,
+            int? idade = null
+        )
+        {
+            return first + " " + last;
         }
     }
 }
