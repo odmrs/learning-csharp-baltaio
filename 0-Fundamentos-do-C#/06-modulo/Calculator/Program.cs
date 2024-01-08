fcbc7d0d1ccd68e1e00b2542ccca3be1d438b5d2
odmrs <omarcosviniciusdev@gmail.com>
2024-01-08 18:13:54 -0300

    First part of the calculator project

---
+++ 0-Fundamentos-do-C#/06-modulo/Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Clean
+            Console.Clear();
+            Menu();
+        }
+
+        static float Sum(float x1, float x2)
+        {
+            return x1 + x2;
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("What do you need?");
+            Console.Write("\n[1]Sum\n[2]Sub\n[3]Mult\n[4]Div\n[5]Exit\nAnswer: ");
+            int answer = int.Parse(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 1:
+                    Console.WriteLine("---------- SUM ----------");
+                    Console.Write("\nEnter first number: ");
+                    float x1 = float.Parse(Console.ReadLine());
+                    Console.Write("Enter the second number: ");
+                    float x2 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("The sum between: " + x1 + " + " + x2 + " = " + Sum(x1, x2));
+                    break;
+            }
+
+        }
+    }
+
+}
