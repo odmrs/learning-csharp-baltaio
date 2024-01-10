a0ac64e1c27e45b79b0de7e1b1e4282c1d81a07e
odmrs <omarcosviniciusdev@gmail.com>
2024-01-10 01:12:05 -0300

    debug stuffs

--- 0-Fundamentos-do-C#/06-modulo-project/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ 0-Fundamentos-do-C#/06-modulo-project/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09979a9918fa5a886d504c354424c1c5459c7964")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 0-Fundamentos-do-C#/07-modulo-project/StopWatch/Program.cs
+++ 0-Fundamentos-do-C#/07-modulo-project/StopWatch/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+
 namespace StopWatch
 {
   class Program
