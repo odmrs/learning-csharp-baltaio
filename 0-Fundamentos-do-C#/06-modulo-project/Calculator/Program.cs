39869c8b7e14dbafa5b2cb67374d318eddee5f85
odmrs <omarcosviniciusdev@gmail.com>
2024-01-09 21:30:36 -0300

    Add more functions to calculator: Square, Cube, Elevate

--- 0-Fundamentos-do-C#/06-modulo-project/Calculator/Program.cs
+++ 0-Fundamentos-do-C#/06-modulo-project/Calculator/Program.cs
@@ -55,26 +55,70 @@ namespace Calculator
         static void Bye()
         {
             Console.WriteLine("Thank's for use my calculator !!!");
-            Thread.Sleep(10000);
+            Thread.Sleep(1300);
             Console.Clear();
             System.Environment.Exit(0);
+        }
+
+        static void Square()
+        {
+            Console.WriteLine("---------- Square ----------");
+            Console.Write("\nEnter the number to see it squared: ");
+            float number = float.Parse(Console.ReadLine());
+            Console.WriteLine($"The number {number}² = {number * number}");
+        }
+
+        static void Cube()
+        {
+            Console.WriteLine("---------- Cube ----------");
+            Console.Write("\nEnter the number to see it cubed: ");
+            float number = float.Parse(Console.ReadLine());
+            Console.WriteLine($"The number {number}³ = {number * number * number}");
+        }
+
+        static void Elevated()
+        {
+            Console.WriteLine("---------- Elevate ----------");
+            Console.Write("\nEnter the base number: ");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.Write("Enter the high number: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float? result = 1;
+
+            if (num2 != 1)
+            {
+                for (int i = 0; i < num2; i++)
+                {
+                    result *= num1; // Multiply (num1) 
+                }
+            }
+            else
+            {
+                result = num1;
+            }
+
+            Console.WriteLine($"\nThe number {num1} ^ {num2} = {result}");
         }
         static void Menu()
         {
             while (true)
             {
                 Console.WriteLine("What do you need?");
-                Console.Write("\n[1]Sum\n[2]Sub\n[3]Mult\n[4]Div\n[5]Exit\n\nAnswer: ");
+                Console.Write("\n[0]Exit\n[1]Sum\n[2]Sub\n[3]Mult\n[4]Div\n[5]Square\n[6]Cube\n[7]Elevated yourself\n\nAnswer: ");
                 int answer = int.Parse(Console.ReadLine());
                 Console.Clear();
 
                 switch (answer)
                 {
+                    case 0: Bye(); break;
                     case 1: Sum(); break;
                     case 2: Sub(); break;
                     case 3: Mult(); break;
                     case 4: Div(); break;
-                    case 5: Bye(); break;
+                    case 5: Square(); break;
+                    case 6: Cube(); break;
+                    case 7: Elevated(); break;
                     default:
                         Console.WriteLine("None of the option exist");
                         break;
--- 0-Fundamentos-do-C#/06-modulo-project/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ 0-Fundamentos-do-C#/06-modulo-project/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d099b45c05fbb43b934c3f19d68f2545809a490c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
