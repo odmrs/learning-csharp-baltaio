5340add6e93bbc08ffa60545863d6e1e4b890f0c
odmrs <omarcosviniciusdev@gmail.com>
2024-01-13 02:25:17 -0300

    add toupperExemple for the documentation Html Editor

--- 0-Fundamentos-do-C#/10-modulo-project/HtmlEditor/Documentation.cs
+++ 0-Fundamentos-do-C#/10-modulo-project/HtmlEditor/Documentation.cs
@@ -12,7 +12,7 @@ namespace HtmlEdit
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            Menu.DrawScreen(50);
+            Menu.DrawScreen(50, 15);
             WriteOptions();
 
             short opition = short.Parse(Console.ReadLine());
@@ -31,11 +31,11 @@ namespace HtmlEdit
             Console.WriteLine("Select an opition bellow to see an exemple:");
             Console.SetCursorPosition(3, 7);
             Console.WriteLine("1 - <strong> </strong>");
-            // Console.SetCursorPosition(3, 8);
-            // Console.WriteLine("2 - Open File");
             Console.SetCursorPosition(3, 8);
+            Console.WriteLine("2 - <toupper> </toupper>");
+            Console.SetCursorPosition(3, 9);
             Console.WriteLine("0 - Exit");
-            Console.SetCursorPosition(3, 10);
+            Console.SetCursorPosition(3, 11);
             Console.Write("Option: ");
         }
 
@@ -45,17 +45,19 @@ namespace HtmlEdit
             {
                 case 0: Menu.Show(); break;
                 case 1: Open.See(Paths(1)); break;
+                case 2: Open.See(Paths(2)); break;
             }
         }
 
         public static string Paths(short option)
         {
             var strongExemple = "/home/odmrs/learning-csharp-baltaio/0-Fundamentos-do-C#/10-modulo-project/HtmlEditor/Exemples/strongExemple.txt";
-
+            var toupperExemple = "/home/odmrs/learning-csharp-baltaio/0-Fundamentos-do-C#/10-modulo-project/HtmlEditor/Exemples/toupperExemple.txt";
 
             switch (option)
             {
                 case 1: return strongExemple;
+                case 2: return toupperExemple;
             }
 
             return "error";
