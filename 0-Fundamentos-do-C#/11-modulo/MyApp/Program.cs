2300552771d5f74074f87092daaa59462f4f9e77
odmrs <omarcosviniciusdev@gmail.com>
2024-01-14 16:41:24 -0300

    Start modulo 11 and add some notes

---
+++ 0-Fundamentos-do-C#/11-modulo/MyApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var date = new DateTime(); | Just inicialized but came with deafult numbers 1/1/0001 - 12:00 AM
+            // var date = DateTime.Now; | busca a data da atual
+            // var date = new DateTime(2024, 1, 14, 15, 25, 58);
+            // To get each one of this results:
+            // Console.WriteLine(date.AddDays(3));
+            // Console.WriteLine(date);
+
+            // FOrmatation
+
+            var date = DateTime.Now;
+            var formated = string.Format("{0:yyyy:MM:dd}", date);
+
+            Console.WriteLine(formated);
+        }
+    }
+}
