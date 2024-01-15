a2c45ce90d40718c93c4dff5f8e8660071aa8fbb
odmrs <omarcosviniciusdev@gmail.com>
2024-01-15 16:48:50 -0300

    add modulo 13 and notes about arrays

---
+++ 0-Fundamentos-do-C#/13-modulo/MyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[5]; // start always with 0
+            // var arr = new int[5] {1,2,3,4,5}; // or inicialized them with {}
+            arr[0] = 44;
+            // Console.WriteLine(arr.Length);
+
+            // Percorrendo arrays
+
+            // for (var i = 0; i < arr.Length; i++)
+            // {
+            //     Console.WriteLine(arr[i]);
+            // }
+
+            // For Each
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
